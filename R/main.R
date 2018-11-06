26d7d20b668c2d30ba71f873042eb22c2360a137
schalkdaniel <d-schalk@t-online.de>
2018-11-06 16:50:04 +0100

    update talk

--- R/main.R
+++ R/main.R
@@ -8,7 +8,7 @@ source("R/data_generator.R")
 library(tidyverse)
 library(ggthemes)
 
-# IID Dataset
+# IID Setting
 # ------------------------------------------
 
 set.seed(3141592)
@@ -20,23 +20,70 @@ y = data$y
 idx = sample(1:4, nrow(X), replace = TRUE)
 index_list = lapply(1:4, function (x) { idx == x })
 
-# Non-IID Dataset
-# ------------------------------------------
-
-# cat_var = cut(iris$Sepal.Width, breaks = c(-Inf, quantile(iris$Sepal.Width, c(0.33, 0.67)), Inf), labels = c(1,2,3))
-
-# index_list = list(which(cat_var == 1), which(cat_var == 2), which(cat_var == 3))
-
-# Fit models and create images:
-# ------------------------------------------
-
-max_iters = 6000L
+max_iters = 100000L
 vec_learning_rates = c(0.001, 0.01, 0.05, 0.1)
 vec_iters_at_once = c(1, 10, 100, 1000, 2000)
 true_beta = data$params[-1]
 
-fed_logreg = getFittingTrace(X = X, y = y, index_list = index_list, max_iters = max_iters, 
+fed_logreg_iid = getFittingTrace(X = X, y = y, index_list = index_list, max_iters = max_iters, 
   vec_iters_at_once = vec_iters_at_once, vec_learning_rates = vec_learning_rates, 
-  true_beta = true_beta)
+  true_beta = true_beta, eps_for_break = 1e-7)
 
-plot(fed_logreg)
+plot(fed_logreg_iid, plot_title = "Federated Logistic Regression with Simulated IID Situation", show_averaging = TRUE)
+ggsave(filename = "images/fed_logreg_iid.pdf", width = 50, height = 30, units = "cm")
+
+
+# IID Unbalanced Setting
+# ------------------------------------------
+
+set.seed(3141592)
+data = simulateBinaryClassif(nrow = 4000, ncol = 2)
+
+X = data$data
+y = data$y
+
+idx = sample(1:4, nrow(X), replace = TRUE, prob = c(0.2, 0.1, 0.6, 0.1))
+index_list = lapply(1:4, function (x) { idx == x })
+
+max_iters = 100000L
+vec_learning_rates = c(0.001, 0.01, 0.05, 0.1)
+vec_iters_at_once = c(1, 10, 100, 1000, 2000)
+true_beta = data$params[-1]
+
+fed_logreg_iid_ub = getFittingTrace(X = X, y = y, index_list = index_list, max_iters = max_iters, 
+  vec_iters_at_once = vec_iters_at_once, vec_learning_rates = vec_learning_rates, 
+  true_beta = true_beta, eps_for_break = 1e-7)
+
+plot(fed_logreg_iid_ub, plot_title = "Federated Logistic Regression with Simulated IID Situation", show_averaging = TRUE)
+ggsave(filename = "images/fed_logreg_iid_ub.pdf", width = 50, height = 30, units = "cm")
+
+# Non-IID Setting 1
+# ------------------------------------------
+
+# Situation: We now destroy the measurements of one feature of one dataset. This could correspond to 
+#            a damaged sensor i.e. in a hospital. 
+
+set.seed(3141592)
+data = simulateBinaryClassif(nrow = 4000, ncol = 2)
+
+X = data$data
+y = data$y
+
+idx = sample(1:4, nrow(X), replace = TRUE)
+index_list = lapply(1:4, function (x) { idx == x })
+
+# Destroy feature 2 of dataset 3:
+idx_set = 3L
+X[index_list[[idx_set]], 2] = X[index_list[[idx_set]], 2] + 4
+
+max_iters = 100000L
+vec_learning_rates = c(0.001, 0.01, 0.05, 0.1)
+vec_iters_at_once = c(1, 10, 100, 1000, 2000)
+true_beta = data$params[-1]
+
+fed_logreg_set1 = getFittingTrace(X = X, y = y, index_list = index_list, max_iters = max_iters, 
+  vec_iters_at_once = vec_iters_at_once, vec_learning_rates = vec_learning_rates, 
+  true_beta = true_beta, eps_for_break = 1e-7, extract_global_model = extractGlobalModelBayes)
+
+plot(fed_logreg_set1, plot_title = "Federated Logistic Regression with Destroyed Feature of Dataset 3", show_averaging = TRUE)
+ggsave("images/fed_logreg_set1.pdf", width = 50, height = 30, units = "cm")
--- R/plot_federated_logreg.R
+++ R/plot_federated_logreg.R
@@ -4,7 +4,37 @@
 #                                                                                   #
 # ================================================================================= #
 
-getFittingTrace = function (X, y, index_list, max_iters, vec_iters_at_once, vec_learning_rates, true_beta = NULL)
+extractGlobalModel = function (list_coef, weights) 
+{
+  return (
+    unlist(apply(X = do.call(rbind, list_coef), MARGIN = 2, function (x) {
+      weighted.mean(x = x, w = weights)
+    }))
+  )
+}
+
+extractGlobalModelBayes = function (list_coef, weights)
+{
+  centroid = unlist(apply(X = do.call(rbind, list_coef), MARGIN = 2, function (x) {
+    mean.default(x = x)
+  }))
+  sigma = cov(do.call(rbind, list_coef))
+  # variances = mean(unlist(apply(X = do.call(rbind, list_coef), MARGIN = 2, function (x) {
+  #   var(x = x)
+  # })))
+  data_dist = unlist(lapply(list_coef, function (x) {
+    mvtnorm::dmvnorm(x = x, mean = centroid, sigma = sigma)
+  }))
+  return (
+    unlist(apply(X = do.call(rbind, list_coef), MARGIN = 2, function (x) {
+      weighted.mean(x = x, w = weights * data_dist)
+    }))
+  )
+}
+
+
+getFittingTrace = function (X, y, index_list, max_iters, vec_iters_at_once, vec_learning_rates, 
+  true_beta = NULL, eps_for_break = 1e-6, param_initializer = runif, extract_global_model = extractGlobalModel)
 {
   X = as.matrix(X)
 
@@ -18,28 +48,36 @@ getFittingTrace = function (X, y, index_list, max_iters, vec_iters_at_once, vec_
   # Calculate glm on global dataset to get best parameters which we want to learn: 
   coef_glm = glm(y ~ 0 + X, family = binomial)$coef
 
-  pb = txtProgressBar(min = min(vec_iters_at_once), max = max(vec_iters_at_once), style = 3)
+  # pb = txtProgressBar(min = min(vec_iters_at_once), max = max(vec_iters_at_once), style = 3)
 
   datasets       =  lapply(index_list, function (idx) { X[idx, ] })
   responses      =  lapply(index_list, function (idx) { y[idx] })
   data_plot_all  =  data.frame()
+  initial_param        =  param_initializer(ncol(X))
 
   message("\n>> Setting up ", length(datasets), " datasets.")
   message(">> Calculating ", length(vec_iters_at_once) * length(vec_learning_rates), " federated learning models.\n")
+  counter = 1
 
   for (iters_at_once in vec_iters_at_once) {
     for (learning_rate in vec_learning_rates) {
 
-      setTxtProgressBar(pb, iters_at_once)
+      # setTxtProgressBar(pb, iters_at_once)
 
-      coef_gd       =  rep(0, ncol(X))
-      global_model  =  coef_gd
-      list_coef     =  lapply(index_list, function (dummy) { coef_gd })
-      iterations    =  seq_len(trunc(max_iters / iters_at_once) + 1)
+      
+      global_model  =  initial_param
+      list_coef     =  lapply(index_list, function (dummy) { initial_param })
+      iterations    =  seq_len(trunc(max_iters / iters_at_once))
       real_iters    =  numeric(2 * length(iterations) + 1)
-      trace_coef    =  lapply(index_list, function (dummy) { matrix(NA, nrow = 2 * length(iterations) + 1, ncol = length(coef_gd)) })
+      trace_coef    =  lapply(index_list, function (dummy) { matrix(NA, nrow = 2 * length(iterations) + 1, ncol = length(initial_param)) })
 
       insert_index = 1
+
+      reached_early_stopping = FALSE
+
+      message(">> ", length(vec_iters_at_once) * length(vec_learning_rates), "/", counter, ": Learning Rate = ", 
+        learning_rate, ", Iters at once = ", iters_at_once)
+      counter = counter + 1
 
       for (i in iterations) {
 
@@ -59,14 +97,23 @@ getFittingTrace = function (X, y, index_list, max_iters, vec_iters_at_once, vec_
         real_iters[insert_index] = i * iters_at_once
 
         # Apply Federated Averaging:
-        global_model = unlist(apply(X = do.call(rbind, list_coef), MARGIN = 2, function (x) {
-          weighted.mean(x = x, w = unlist(lapply(responses, length)))
-        }))
+        global_model = extract_global_model(list_coef, weights = unlist(lapply(responses, length)))
 
-        if (i == last(iterations)) {
+        global_model_dist = sqrt(sum((global_model - trace_coef[[1]][insert_index - 2, ])^2))
+        if (global_model_dist < eps_for_break) {
+          reached_early_stopping = TRUE
+        }
+
+        if (i == last(iterations) || reached_early_stopping) {
           for (k in seq_along(datasets)) {
             trace_coef[[k]][insert_index, ] = global_model
           }
+        }
+
+        if (reached_early_stopping) { 
+          nbr_spaces = nchar(paste0(">> ", length(vec_iters_at_once) * length(vec_learning_rates), "/", counter, ": "))
+          message(rep(" ", nbr_spaces), "Break fitting process after iteration ", i * iters_at_once)
+          break 
         }
       }
 
@@ -76,16 +123,16 @@ getFittingTrace = function (X, y, index_list, max_iters, vec_iters_at_once, vec_
         data_traces[[k]]$step  =  c(rep(c("Averaging", "Update"), times = length(iterations)), "Averaging")
         data_traces[[k]]$iter  =  real_iters
       }
-      data_traces = do.call(rbind, data_traces)
+      data_traces = na.omit(do.call(rbind, data_traces))
       data_traces$learning_rate = learning_rate
       data_traces$iters_at_once = iters_at_once
 
-      names(data_traces) = c("Intercept", "Slope", "Dataset", "Step", "Iteration", "LearningRate", "ItersAtOnce")
+      names(data_traces) = c("Feature1", "Feature2", "Dataset", "Step", "Iteration", "LearningRate", "ItersAtOnce")
 
       data_plot_all = rbind(data_plot_all, data_traces)
     }
   }
-  close(pb)
+  # close(pb)
 
   # Calculate single glm models:
   glm_single = list()
@@ -106,7 +153,7 @@ getFittingTrace = function (X, y, index_list, max_iters, vec_iters_at_once, vec_
 
 
 
-plot.fedLearn = function (obj, ...)
+plot.fedLearn = function (obj, plot_title = "Federated Learning with Logistic Regression", show_averaging = FALSE)
 {
   X              =  obj[["data"]]
   data_plot_all  =  obj[["trace"]]
@@ -115,18 +162,28 @@ plot.fedLearn = function (obj, ...)
   max_iters      =  obj[["max_iters"]]
   true_beta      =  obj[["true_beta"]]
 
+  data_iters = data_plot_all %>% 
+    group_by(ItersAtOnce, LearningRate) %>% 
+    filter(row_number() == n()) %>% 
+    select(Iteration) %>% 
+    mutate(Iteration = paste0("Trained Iterations: ", Iteration)) %>%
+    group_by(ItersAtOnce, LearningRate)
+
   # Get final points per dataset:
   final_points = data_plot_all %>% 
     group_by(Dataset, ItersAtOnce, LearningRate) %>%
     filter(row_number() == n()) %>%
     group_by(ItersAtOnce, LearningRate) %>%
-    summarize(Intercept = mean(Intercept), Slope = mean(Slope))
+    summarize(Feature1 = mean(Feature1), Feature2 = mean(Feature2))
 
   data_all = data_plot_all %>% 
-    group_by(Dataset, ItersAtOnce, LearningRate) %>%
-    filter(Step == "Update" | row_number() == 1) # %>%
-    # filter(row_number() %in% seq(1, n(), by = 1))
+    group_by(Dataset, ItersAtOnce, LearningRate) 
 
+  if (! show_averaging) {
+    data_all = data_all %>%
+      filter(Step == "Update" | row_number() == 1) # %>%
+      # filter(row_number() %in% seq(1, n(), by = 1))
+  }
   data_all_final = data_plot_all %>% 
     group_by(Dataset, ItersAtOnce, LearningRate) %>%
     filter(Step == "Update") %>%
@@ -138,15 +195,15 @@ plot.fedLearn = function (obj, ...)
     # group_by(ItersAtOnce, LearningRate) %>%
     # filter(row_number() %in% seq(1, n(), by = 1))
 
-  plot_range1 = c(min(c(data_all$Intercept, glm_single$Feature1)), max(c(data_all$Intercept, glm_single$Feature1)))
+  plot_range1 = c(min(c(data_all$Feature1, glm_single$Feature1, true_beta[1])), max(c(data_all$Feature1, glm_single$Feature1, true_beta[1])))
   margin1 = abs(diff(plot_range1)) * 0.1
-  plot_range2 = c(min(c(data_all$Slope, glm_single$Feature2)), max(c(data_all$Slope, glm_single$Feature2)))
+  plot_range2 = c(min(c(data_all$Feature2, glm_single$Feature2, true_beta[2])), max(c(data_all$Feature2, glm_single$Feature2, true_beta[2])))
   margin2 = abs(diff(plot_range2)) * 0.1
 
   # Calculate data frame with the surface:
   data_surface = expand.grid(
-    Intercept = seq(plot_range1[1] - margin1, plot_range1[2] + margin1, length.out = 100), 
-    Slope = seq(plot_range2[1] - margin2, plot_range2[2] + margin2, length.out = 100)
+    Feature1 = seq(plot_range1[1] - margin1, plot_range1[2] + margin1, length.out = 100), 
+    Feature2 = seq(plot_range2[1] - margin2, plot_range2[2] + margin2, length.out = 100)
     )
   data_surface$risk = apply(data_surface, 1, function (params) { 
     empiricalRisk(
@@ -155,32 +212,33 @@ plot.fedLearn = function (obj, ...)
   })
 
   gg = ggplot() + 
-    geom_raster(data = data_surface, aes(Intercept, Slope, fill = risk), show.legend = TRUE) +
-    geom_contour(data = data_surface, aes(Intercept, Slope, z = risk), colour = "white", alpha = 0.5, size = 0.5) +
-    geom_path(data = data_all, aes(x = Intercept, y = Slope, colour = Dataset), alpha = 0.5) + 
-    geom_point(data = data_all_final, aes(x = Intercept, y = Slope, colour = Dataset), shape = 4, show.legend = FALSE) + 
-    geom_path(data = data_global_model, aes(x = Intercept, y = Slope), colour = "white") + 
-    geom_point(data = final_points, aes(x = Intercept, y = Slope), colour = "white", shape = 4, size = 2) +
-    geom_text(data = final_points, aes(x = Intercept, y = Slope, label = "FedAvg"), colour = "white", vjust = -1) +
+    geom_raster(data = data_surface, aes(x = Feature1, y = Feature2, fill = risk), show.legend = TRUE) +
+    geom_contour(data = data_surface, aes(x = Feature1, y = Feature2, z = risk), colour = "white", alpha = 0.5, size = 0.5) +
+    geom_path(data = data_all, aes(x = Feature1, y = Feature2, colour = Dataset), alpha = 0.5) + 
+    geom_point(data = data_all_final, aes(x = Feature1, y = Feature2, colour = Dataset), shape = 4, show.legend = FALSE) + 
+    geom_path(data = data_global_model, aes(x = Feature1, y = Feature2), colour = "white") + 
+    geom_point(data = final_points, aes(x = Feature1, y = Feature2), colour = "white", shape = 4, size = 2) +
+    geom_text(data = final_points, aes(x = Feature1, y = Feature2, label = "FedAvg"), colour = "white", vjust = -1) +
     annotate("point", x = coef_glm[1], y = coef_glm[2], colour = rgb(205, 150, 205, 255, maxColorValue = 255), shape = 4, size = 2) + 
     annotate("text", x = coef_glm[1], y = coef_glm[2], label = "GLM", vjust = -1, colour = rgb(205, 150, 205, 255, maxColorValue = 255)) + 
     geom_point(data = glm_single, aes(x = Feature1, y = Feature2, colour = dataset), show.legend = FALSE) +
+    geom_text(data = data_iters, aes(x = -Inf, y = Inf, label = Iteration), colour = "black", hjust = 0, vjust = 1) + #, hjust = 0.5, vjust = 1.2) +
     theme_tufte() +
     scale_color_brewer(palette = "Spectral") +
     facet_grid(rows = vars(ItersAtOnce), cols = vars(LearningRate)) +
-    labs(title = "Centralized vs. Decentralized Learning", 
-     subtitle = paste0("Example using Logistic Regression and ", max_iters, " iterations")) +
+    labs(title = plot_title, subtitle = paste0("Maximal number of possible iterations: ", max_iters)) +
     xlab("Feature 1") +
     ylab("Feature 2") +
     xlim(plot_range1[1] - margin1, plot_range1[2] + margin1) +
-    ylim(plot_range2[1] - margin2, plot_range2[2] + margin2) +
-    annotate("point", x = 0, y = 0, colour = "white")
+    ylim(plot_range2[1] - margin2, plot_range2[2] + margin2 * 1.8) +
+    annotate("point", x = data_plot_all[1,1], y = data_plot_all[1,2], colour = "white") +
+    guides(colour = guide_legend(override.aes = list(fill = NA))) +
+    theme(legend.position = "bottom", panel.background = element_blank())
 
-  if (is.null(true_beta[1])) {
+  if (! is.null(true_beta[1])) {
     gg = gg + 
       annotate("point", x = true_beta[1], y = true_beta[2], colour = rgb(164, 211, 238, 255, maxColorValue = 255), shape = 4, size = 2) + 
-      annotate("text", x = true_beta[1], y = true_beta[2], label = "GLM", vjust = -1, colour = rgb(164, 211, 238, 255, maxColorValue = 255))
+      annotate("text", x = true_beta[1], y = true_beta[2], label = "True Parameter", vjust = -1, colour = rgb(164, 211, 238, 255, maxColorValue = 255))
   }
-
   return (gg)
 }
